15af8c8afd033e39e9e3fe8c980219ab60a3f1be
Eunchurn Park <eunchurn.park@gmail.com>
2022-04-16 11:49:07 +0900

    fix(twitter-card): replace

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -11,7 +11,7 @@ const siteMetadata = {
   siteRepo: "https://github.com/eunchurn/eunchurn.com",
   siteLogo: "/static/images/logo.png",
   image: "/static/images/avatar.png",
-  socialBanner: "/static/images/twitter-card.png",
+  socialBanner: "/static/images7800413.jpeg",
   email: "eunchurn.park@gmail.com",
   github: "https://github.com/eunchurn",
   twitter: "https://twitter.com/eunchurn",
