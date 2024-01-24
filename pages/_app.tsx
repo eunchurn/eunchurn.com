a37538b0187231641535182417f5eaf83f22634a
Eunchurn Park <eunchurn.park@gmail.com>
2024-01-24 14:35:20 +0900

    feat(cv): added fron Notion CMS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import "@/css/tailwind.css";
 import "@/css/prism.css";
 import "@/css/vimeo.css";
 import "katex/dist/katex.css";
-
+import "styles/root.css";
+import "styles/notion.css";
 // import "@fontsource/inter/variable-full.css";
 
 import { ThemeProvider } from "next-themes";
@@ -13,6 +14,13 @@ import siteMetadata from "@/data/siteMetadata";
 import Analytics from "@/components/analytics";
 import LayoutWrapper from "@/components/LayoutWrapper";
 import { ClientReload } from "@/components/ClientReload";
+
+import localFont from "next/font/local";
+
+const pretendard = localFont({
+  src: "./PretendardVariable.woff2",
+  display: "swap",
+});
 
 const isDevelopment = process.env.NODE_ENV === "development";
 const isSocket = process.env.SOCKET;
@@ -26,7 +34,9 @@ export default function App({ Component, pageProps }: AppProps) {
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
-        <Component {...pageProps} />
+        <main className={pretendard.className}>
+          <Component {...pageProps} />
+        </main>
       </LayoutWrapper>
     </ThemeProvider>
   );
