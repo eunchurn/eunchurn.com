ec14701d7e41651e711d99600c2316195e5bc42c
Eunchurn Park <eunchurn.park@gmail.com>
2024-01-24 16:05:52 +0900

    fix(button): in mdx

--- data/authors/default.md
+++ data/authors/default.md
@@ -11,3 +11,12 @@ Ph.D.
 한때는 엔지니어였고, 음악을 하는 사람이었으며, 개발자이기도 합니다.
 
 all of works archive of Eunchurn Park - works diary, [music](/categories/music), [saram12saram](/tags/saram12saram), [remixes](/tags/remix), [reworks](/tags/rework), [runway project](/categories/runway), [production note](/tags/note), [interview archive](/categories/press), [engineering](/categories/engineering), [development](/categories/development) and engineering research archive)
+
+<div className="container flex justify-center">      <button
+        type="button"
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-gray-300 px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-32 ml-24 mt-3 inline-block rounded bg-primary pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:ring-4 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]
+        "
+        onClick={() => { window.location.href = "/cv" }}
+      >
+        CV/Résumé
+      </button></div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ import { MDXLayoutRenderer } from "@/components/MDXComponents";
 import { getFileBySlug } from "@/lib/mdx";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { AuthorFrontMatter } from "types/AuthorFrontMatter";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 const DEFAULT_LAYOUT = "AuthorLayout";
 
@@ -19,7 +19,7 @@ export default function About({
   authorDetails,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const { mdxSource, frontMatter } = authorDetails;
-  const router = useRouter();
+  // const router = useRouter();
   return (
     <div className="flex flex-col">
       <MDXLayoutRenderer
@@ -27,14 +27,14 @@ export default function About({
         mdxSource={mdxSource}
         frontMatter={frontMatter}
       />
-      <button
+      {/* <button
         type="button"
         className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-gray-300 px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-32 ml-24 mt-3 inline-block rounded bg-primary pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:ring-4 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]
         "
         onClick={() => router.push("/cv")}
       >
         CV/Résumé
-      </button>
+      </button> */}
     </div>
   );
 }
