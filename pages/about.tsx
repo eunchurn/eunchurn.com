3a326df85d9be5fa71390b8d97d28665ac3c099c
Eunchurn Park <eunchurn.park@gmail.com>
2024-01-24 15:23:54 +0900

    chore(cv): button added in about

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import { MDXLayoutRenderer } from "@/components/MDXComponents";
 import { getFileBySlug } from "@/lib/mdx";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { AuthorFrontMatter } from "types/AuthorFrontMatter";
+import { useRouter } from "next/navigation";
 
 const DEFAULT_LAYOUT = "AuthorLayout";
 
@@ -18,12 +19,21 @@ export default function About({
   authorDetails,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const { mdxSource, frontMatter } = authorDetails;
-
+  const router = useRouter();
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
+    <div className="flex flex-col">
+      <MDXLayoutRenderer
+        layout={frontMatter.layout || DEFAULT_LAYOUT}
+        mdxSource={mdxSource}
+        frontMatter={frontMatter}
+      />
+      <button
+        type="button"
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-32 ml-24 mt-3"
+        onClick={() => router.push("/cv")}
+      >
+        CV/Résumé
+      </button>
+    </div>
   );
 }
